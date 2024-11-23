931e75eed2d2eb15325f8a6543789ad842d66b78
vijaymksaxena <vijaymksaxena@gmail.com>
2024-11-23 14:24:12 +0530

    fixed css

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 
-$(document).ready(function(){
-    $("#heart").click(function(){
-      if($("#heart").hasClass("liked")){
-        $("#heart").html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
-        $("#heart").removeClass("liked");
-      }else{
-        $("#heart").html('<i class="fa fa-heart" aria-hidden="true"></i>');
-        $("#heart").addClass("liked");
-      }
-    });
+
+  document.querySelector(".menu-toggle").click(function () {
+    console.log("clicked")
+    $(this).attr("data-open", (_, attr) => (attr == "true" ? "false" : "true"));
   });
+  
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const togglers = document.querySelectorAll('[data-toggle]');
+    
+      togglers.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+           const selector = e.currentTarget.dataset.toggle
+           const block = document.querySelector(`${selector}`);
+          if (e.currentTarget.classList.contains('active')) {
+            block.style.maxHeight = '';
+          } else {
+            block.style.maxHeight = block.scrollHeight + 'px';
+          }
+            
+           e.currentTarget.classList.toggle('active')
+        })
+      })
+    })
